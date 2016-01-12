b7378cc33c080c5e0dd5e758621fb39064232437
Austin Morrell <austinlm19@gmail.com>
2016-01-12 10:53:38 -0600

    0.1

--- Your Project/Cell.cpp
+++ Your Project/Cell.cpp
@@ -1,9 +1,16 @@
 #include "Cell.h"
+<<<<<<< Updated upstream
 
+=======
+#include <stdlib.h>
+>>>>>>> Stashed changes
 
 
 Cell::Cell()
 {
+	Pos((rand() % 1600 + 1), (rand() % 900 + 1));
+	Radius = 20;
+	BC = NewBoundingCircle(Pos, Radius);
 }
 
 
